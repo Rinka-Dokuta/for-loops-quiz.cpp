a77e4d987964467d3be2a5533a8732f8bf2db54a
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-09-09 10:06:37 -0600

    Create for loops quiz.cpp

---
+++ for loops quiz.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i <= 18; i += 1) //for loop that prints out the numbers 0-18. 
+		cout << i << " ";
+
+	cout << endl;
+
+	for (int i = 25; i <= 46; i += 1) //for loop that prints out the numbers 25-45.
+		cout << i << " ";
+	
+	cout << endl;
+
+	for (int i = 40; i <= 80; i += 4) //for loop that prints out the numbers 40 through 80 counting by 4s
+		cout << i << " ";
+
+	cout << endl;
+
+	for (int i = 12; i >= -6; i -= 3) //for loop that prints out the numbers 12 to -6 counting down by -3s
+		cout << i << " ";
+
+	cout << endl;
+
+
+
+
+
+
+
+
+}
